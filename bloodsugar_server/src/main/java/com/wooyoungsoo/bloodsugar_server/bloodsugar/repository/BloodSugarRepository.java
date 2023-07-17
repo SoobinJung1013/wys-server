c7e58e232f8d4dbf02f21868017e305b116dab85
SoobinJung <judyb0626@gmail.com>
2023-07-18 04:44:24 +0900

    feat: Delete unused file

--- bloodsugar_server/src/main/java/com/wooyoungsoo/bloodsugar_server/bloodsugar/repository/BloodSugarRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wooyoungsoo.bloodsugar_server.bloodsugar.repository;
-
-import com.wooyoungsoo.bloodsugar_server.bloodsugar.entity.BloodSugar;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BloodSugarRepository extends JpaRepository<BloodSugar, Long> {
-
-}
