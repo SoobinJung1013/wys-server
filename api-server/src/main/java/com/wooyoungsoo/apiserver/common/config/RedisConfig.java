693af6b21f97e597b42e700a524fdbb3ae8e8219
SoobinJung <judyb0626@gmail.com>
2023-08-01 02:46:59 +0900

    feat: Add redis cache to dog feed api

---
+++ api-server/src/main/java/com/wooyoungsoo/apiserver/common/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.wooyoungsoo.apiserver.common.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Getter
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+@EnableCaching
+public class RedisConfig {
+
+    @Value("${spring.cache.redis.host}")
+    private String host;
+
+    @Value("${spring.cache.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<?,?> redisTemplate(){
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager feedCacheManager(RedisConnectionFactory connectionFactory) {
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofMinutes(3L));
+
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory).cacheDefaults(redisCacheConfiguration).build();
+    }
+}
--- api-server/src/main/java/com/wooyoungsoo/apiserver/feed/controller/FeedController.java
+++ api-server/src/main/java/com/wooyoungsoo/apiserver/feed/controller/FeedController.java
@@ -3,11 +3,15 @@ package com.wooyoungsoo.apiserver.feed.controller;
 import com.wooyoungsoo.apiserver.feed.dao.DogFeedDAO;
 import com.wooyoungsoo.apiserver.feed.entity.DogFeed;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/feeds")
@@ -19,8 +23,18 @@ public class FeedController {
         this.dogFeedDAO = dogFeedDAO;
     }
 
+    @GetMapping("/{id}")
+    @Cacheable(value = "dog_feeds", key = "#id")
+    public DogFeed getFeedById(@PathVariable String id) {
+        return dogFeedDAO.findById(id).orElse(null);
+    }
+
     @GetMapping
+    @Cacheable("popular_dog_feeds")
     public List<DogFeed> getAllFeeds() {
-        return dogFeedDAO.findAll();
+//        return dogFeedDAO.findAll(Sort.by(Sort.Direction.ASC, "rank")).stream()
+        return dogFeedDAO.findAll().stream()
+                .limit(100)
+                .collect(Collectors.toList());
     }
 }
